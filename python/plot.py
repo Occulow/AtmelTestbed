de3dc759b6dd1149c3c971637375f7348278f090
Terence Sun <tsun1215@gmail.com>
2017-03-06 13:25:17 -0500

    Implemented writing median filtered matrix to file over time

--- python/plot.py
+++ python/plot.py
@@ -23,23 +23,32 @@ im.set_clim(0,10)
 
 ser.open()
 
-fname = str(int(round(time.time() * 1000))) + ".log.csv"
+t = str(int(round(time.time() * 1000))) 
+fname = t + ".log.csv"
+fname_med = t + "-median" + ".log.csv"
 
 # current_time = datetime.datetime.now()
-with open(fname, 'w') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    while True:
-      data = ser.readline()
-      frame = numpy.fromstring(data, sep=",").reshape((8,8)) * 0.25
-      
-      # Write to csv
-      writer.writerow(frame.flatten())
+with open(fname_med, 'w') as med_file:
+  with open(fname, 'w') as csv_file:
+      writer = csv.writer(csv_file, delimiter=',')
+      writer_med = csv.writer(med_file, delimiter=',')
+      while True:
+        data = ser.readline()
+        frame = numpy.fromstring(data, sep=",").reshape((8,8)) * 0.25
+        
+        # Write to csv
+        writer.writerow(frame.flatten())
 
-      history = numpy.dstack((frame, history))[:,:,0:MEDIAN_FILTER_SIZE]
-      im.set_array(frame - numpy.median(history, axis=2))
-      fig.canvas.draw()
-      # Print FPS
-      #current_time = datetime.datetime.now()
-      #print str(1000000/(current_time - last_time).microseconds) + " FPS"
-      #last_time = current_time
+        history = numpy.dstack((frame, history))[:,:,0:MEDIAN_FILTER_SIZE]
+
+        filtered_val = frame - numpy.median(history, axis=2)
+        im.set_array(filtered_val)
+
+        writer_med.writerow(filtered_val.flatten())
+
+        fig.canvas.draw()
+        # Print FPS
+        #current_time = datetime.datetime.now()
+        #print str(1000000/(current_time - last_time).microseconds) + " FPS"
+        #last_time = current_time
   
